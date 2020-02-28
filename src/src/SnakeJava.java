e49041a54efd1f0e862ed9b32aeff860ce19b75b
alequintana02 <aquintana5@miners.utep.edu>
2020-02-28 16:39:33 -0700

    committing because i have to downgrade from Junit 52 to 51

--- src/src/SnakeJava.java
+++ src/src/SnakeJava.java
@@ -8,93 +8,117 @@ public class SnakeJava {
     private int[] headPosition;//size =2;
     private static int exhaustiveChecks;
     private static int recursiveChecks;
-    private int headX;
-    private int headY;
+
 
 
                                                     //constructors//
 // default constructor
     public SnakeJava() {
-        this.game = new boolean[1][1];
+        game = new boolean[1][1];
     }//closes default constructor size 1x1
 
     public SnakeJava(boolean[][] game, int headX, int headY) {
+        this.game= game;
+        headPosition[0]=headX;
+        headPosition[1]=headY;
     }//closes hP constructor
                                                  //end of constructors//
 
 
                                                         //methods//
 
-
-    private int neighbors(boolean[][] game) {//counts LIVE neighbors//make sure to not go out of bounds
-        int neighbors = 0;
-        for (int r = 0; r < game.length; r++) {
-            for (int c = 0; c < game[r].length; c++) {
-                if (game[r][c] == true){
-                    neighbors++;
-                }if (game[r][c - 1] == true) {//left
-                    neighbors++;
-                } if (game[r - 1][c] == true) {//straight up
-                    neighbors++;
-                } if (game[r + 1][c] == true) {//straight down
-                    neighbors++;
-                } if (game[r][c + 1] == true) {//right
-                    neighbors++;
+//helper method to count neighbors//
+    private int neighbor(int r, int c) {//counts LIVE neighbors//make sure to not go out of bounds
+        int neighbor = 0;
+       // for (int r = 0; r < game.length; r++) {
+        //    for (int c = 0; c < game[r].length; c++) {
+                if (game[r][c - 1]) {//left
+                    neighbor++;
+                } if (game[r - 1][c]) {//straight up
+                    neighbor++;
+                } if (game[r + 1][c]) {//straight down
+                    neighbor++;
+                } if (game[r][c + 1]) {//right
+                    neighbor++;
                 }
-            }//closes col }//closes for loop
-        }//closes neighbors
-        return neighbors;
-    }//
+            //}//closes col }//closes for loop
+        //}//closes neighbors
+        return neighbor;
+    }//ends helper method
 
 
     //will find the tail of the snake by searching across the whole grid to find the grid position where a true element is
 // surrounded by only one other true cell (see figure below), but is not the head, and return 3 items: the x and y
 // position of the tail in the grid, and the length of the snake on the board. Increments exhaustiveChecks counter with each (x',y') cell that is examined.
-    public int[] findTailExhaustive(int neighbors) {
+    public int[] findTailExhaustive() {
         resetCounters();//need to reset counters at beginning of method
         //neighbors needs to be less than or equal to three
         int result[] = new int[3];//result will give back 3 values, (x&y)position and total length
         int length = 0;
-        int tailX = -1;
-        int tailY = -1;
+        int tailX;
+        int tailY;
         //intTotalLength = 0;
-        //if neighbor=1 it is either tail or head.
-        //if neigh=2 or more, move to the next cell
         for (int r = 0; r < game.length; r++) {//row traversal
             for (int c = 0; c < game[r].length; c++) {//col traversal
-                if (game[r][c] == true && neighbors == 1) {//we want to make sure the cell is true and has at most 1 true neighbor= head or tail
-                    length++;//THE HEAD ONLY HAS ONE NEIGHBOR
-                   // headX = r;
-                   // headY = c;
-                }//closes if statement
-                else if (game[r][c] == true && neighbors !=1) {
-                    length++;
-                    tailX=r;
-                    tailY=c;
-                    result[0] = tailX;
-                    result[1] = tailY;
-                    result[2] = length;
-                    exhaustiveChecks--;
-                }
                 exhaustiveChecks++;
-            }//closes row
-        }//closes col
-        return result;
-        //(x,y position);
+                if (game[r][c]) {//we want to make sure the cell is true
+                    length++;//increasing length because it is part of the snake THE HEAD ONLY HAS ONE NEIGHBOR
+                //}//closes if statement
+                int neighbors=neighbor(r,c); //using neighbor method to count neighbors
+                if(neighbors == 1) {//could be head or could be tail
+                    if (!game[headPosition[0]][headPosition[1]]) {
+                        tailX = r;
+                        tailY = c;
+                        result[0] = tailX;
+                        result[1] = tailY;
+                    }
+                  }result[2] = length;
+                }
+            }//closes col
+        }//closes row
+        return result; //array with (x,y, length);
     }//closes findTailExhaustive
 
 
 
-        public int[] findTailRecursive () {// will return the exact same thing as exhaustive
-                return recursiveChecks;
-            }//closes findTailRecursive
+        public int[] findTailRecursive () {
+        resetCounters();//2 lines of code total for this method
+        return findTailRecursive(headPosition, headPosition);//hint given in readme
+        }//closes findTailRecursive
 
 //
-        private int[] findTailRecursive (int[] currentPosition, int[] previousPosition){
-        resetCounters();//2 lines of code total for this method, rC counts as one so i am missing another line
+        private int[] findTailRecursive (int[] currentPosition, int[] previousPosition) {
+            // will return the exact same thing as exhaustive
+            int result[] = new int[3];//result will give back (x&y)position and total length
+            int length = 1;//we are starting at a place other than head
+            int tailX = 0;
+            int tailY = 0;
+            int temp[];
+            int neighborsCurrent = neighbor(currentPosition[0], currentPosition[1]);//runs neighbor method with current position
+            recursiveChecks++;
+            //if neighbor=1 it is either tail or head. here we assume it is the head
+             if (neighborsCurrent == 1 && currentPosition == previousPosition) {
+                temp = currentPosition;
+                currentPosition[0]++;
+                currentPosition[1]++;
+                previousPosition = temp;
+            } if(neighborsCurrent>=2) { //if neigh=2 or more, move to the next cell
+                length++;
+                findTailRecursive();
+           //if neighbor=1 it is either tail or head. here we assume it is the tail
+            } if (neighborsCurrent==1 && currentPosition!=previousPosition){
+            tailX=currentPosition[0];
+            tailY=currentPosition[1];
+            } else {//keep looking
+        findTailRecursive();
+            }
+            result[0] = tailX;
+            result[1] = tailY;
+            result[2] = length;
+            return result;
 
-            return findTailRecursive;
-        }//closes private findTailRecursive
+        }
+        //closes private findTailRecursive
 
 
     public void resetCounters(){//resets both the exhaustiveChecks and recursiveChecks counters to 0.
--- src/src/SnakeJavaTest.java
+++ src/src/SnakeJavaTest.java
@@ -1,18 +1,25 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-//est the two methods correctness and the running time (at least 5 for each method independently,
+//test the two methods correctness and the running time (at least 5 for each method independently,
 //or 10 that test both methods at once). Each test should have a comment describing what is being tested
 //justify test cases and if it passed or failed
 class SnakeJavaTest {
     @Test
     public void test1(){
-        SnakeJavaTest test = new SnakeJavaTest();
-        assertArrayEquals(SnakeJavaTest.findTailExhaustive());
+        boolean[][]game={
+                {false, false, true},//<- is HP is (0,2)
+                {false, false, true},
+                {false, true, true}};//length should be 4, tail is at (2,1)
+        SnakeJava test = new SnakeJava(game,0,2);
+        int[]result= test.findTailExhaustive();
+        int[]tester=new int[3];
+        tester[0]=2;
+        tester[1]=1;
+        tester[2]=4;
+        assertArrayEquals(tester,result);
     }//closes test1
-
-
     @Test
     public void test2(){
         SnakeJavaTest test = new SnakeJavaTest();
@@ -40,31 +47,32 @@ class SnakeJavaTest {
     @Test
     public void test6(){
         SnakeJavaTest test = new SnakeJavaTest();
-        assertArrayEquals(SnakeJavaTest.findTailExhaustive());
+        assertArrayEquals(SnakeJavaTest.findTailRecursive());
     }//closes test6
 
     @Test
     public void test7(){
         SnakeJavaTest test = new SnakeJavaTest();
-        assertArrayEquals(SnakeJavaTest.findTailExhaustive());
+        assertArrayEquals(SnakeJavaTest.findTailRecursive());
     }//closes test7
 
     @Test
     public void test8(){
         SnakeJavaTest test = new SnakeJavaTest();
-        assertArrayEquals(SnakeJavaTest.findTailExhaustive());
+        assertArrayEquals(SnakeJavaTest.findTailRecursive());
     }//closes test8
 
     @Test
     public void test9(){
         SnakeJavaTest test = new SnakeJavaTest();
-        assertArrayEquals(SnakeJavaTest.findTailExhaustive());
+        assertArrayEquals(SnakeJavaTest.findTailRecursive());
     }//closes test9
 
     @Test
     public void test10(){
         SnakeJavaTest test = new SnakeJavaTest();
-        assertArrayEquals(SnakeJavaTest.findTailExhaustive());
+        assertArrayEquals(SnakeJavaTest.findTailRecursive());
     }//closes test10
+
 
 }//closes Tester class
